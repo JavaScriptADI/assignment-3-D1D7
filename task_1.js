e5133cb69b72110d5e0d09d0e961c1a1db09e8c6
D1D7 <152475650+D1D7@users.noreply.github.com>
2024-02-14 01:09:37 +0400

    Add files via upload

--- task_1.js
+++ task_1.js
@@ -1,13 +1,4 @@
-/* 
-Write a loop that makes seven calls to console.log to output the following triangle:
 
-დაწერეთ ციკლი, რომელიც 7-ჯერ იძახებს console.log-ს რომ გამოიტანოს შემდეგი სამკუთხედი:
-
-#
-##
-### 
-#### 
-##### 
-###### 
-#######
-*/
+for (let i = 1; i <= 10; i++) {
+    console.log('^'.repeat(i));
+  }
